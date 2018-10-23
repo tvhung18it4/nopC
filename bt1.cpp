3f58f6c02dae973604517d907545aab15292bd93
tvhung18it4 <tvhung.18it4@sict.udn.vn>
2018-10-23 17:14:04 +0700

    first commit

---
+++ bt1.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int tamgiac(int n){
+	n;
+	for(int i=1;i<=n;i++){
+		for(int j=1;j<=i;j++){
+			printf("* ");
+		}
+		printf("\n");
+	}
+	return n;
+}
+int main(){
+	int n;
+	printf("Nhap canh tam giac: ");
+	scanf("%d",&n);
+	printf(" ",tamgiac(n));
+}
